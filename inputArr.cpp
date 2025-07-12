1ea4d08160de2c3fd32797db6bdeb57c771c352d
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-12 19:10:50 +0600

    Given a number N and an array A of N numbers. Determine if the number X exists in array A or not and print its position (0-index).

--- inputArr.cpp
+++ inputArr.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 
 int main(){
-    int n;
 
+    int n;
     cin>>n;
 
     int arr[n];
+    
 
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    
+
     for(int i=0;i<n;i++){
-        cout<<" "<<arr[i];
+        cout<<arr[i];
     }
+
+    return 0;
 }
