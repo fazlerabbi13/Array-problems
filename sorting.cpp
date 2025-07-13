7c028b437dada3c0e985ecb560b7a3a4aa18d165
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-13 15:16:13 +0600

    sort the numbers

---
+++ sorting.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int A[N];
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    // Bubble Sort
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - i - 1; j++) {
+            if (A[j] > A[j + 1]) {
+                swap(A[j], A[j + 1]);
+            }
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        cout << A[i] << " ";
+    }
+    return 0;
+}
